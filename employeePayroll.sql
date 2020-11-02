bb4f1bcbf34df0544c4b745812371c23c6a28bf4
shivamj2603 <shivamj2603@gmail.com>
2020-11-02 07:29:37 +0530

    Added: Functionality to create a table

--- employeePayroll.sql
+++ employeePayroll.sql
@@ -2,3 +2,13 @@
 create database payroll_service;
 show databases;
 use payroll_service;
+#Usecase2:
+create table employee_payroll		
+(
+id int not null auto_increment,
+name varchar(150) not null,
+salary double not null,
+start date not null,
+primary key (id)
+);
+describe employee_payroll;
