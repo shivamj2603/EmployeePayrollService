a4300636682bf2863a84ba6fe930fdbe03d7a17b
shivamj2603 <shivamj2603@gmail.com>
2020-11-02 07:43:45 +0530

    Added:Query to add column gender to existing table and set the values

--- employeePayroll.sql
+++ employeePayroll.sql
@@ -2,6 +2,7 @@
 create database payroll_service;
 show databases;
 use payroll_service;
+
 #Usecase2:
 create table employee_payroll		
 (
@@ -12,15 +13,24 @@ start date not null,
 primary key (id)
 );
 describe employee_payroll;
+
 #Usecase 3:
 insert into employee_payroll (name, salary, start)
 values
 ('Bill', 100000, '2018-01-03'),
 ('Terisa', 300000, '2019-12-03'),
 ('Charlie', 500000, '2020-02-03');
+
 #Usecase 4:
 select * from employee_payroll;	
+
 #Usecase 5:
 select salary from employee_payroll where name = 'Bill';
 select * from employee_payroll
 where start between cast('2020-01-01' as date) and date(now());
+
+#Usecase 6:
+alter table employee_payroll add gender char(1)	after name;
+update employee_payroll
+set gender = 'M' where name = 'Bill' or name = 'Charlie';
+update employee_payroll set gender = 'F' where name = 'Terisa';
